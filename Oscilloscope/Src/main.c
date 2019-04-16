6a7b01b61d1e7d2371c2212a3d90416c8d4fde1a
selligZeittom <gilmottiez@gmail.com>
2019-04-16 08:17:20 +0200

    right frequency for timer 1

--- Oscilloscope/Src/main.c
+++ Oscilloscope/Src/main.c
@@ -346,9 +346,9 @@ static void MX_TIM1_Init(void)
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 168;
+  htim1.Init.Prescaler = 84;
   htim1.Init.CounterMode = TIM_COUNTERMODE_DOWN;
-  htim1.Init.Period = 99;
+  htim1.Init.Period = 98;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
