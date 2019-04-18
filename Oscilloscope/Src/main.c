b1f8fb994c4cb26c97f30399e812a0d894825c1c
selligZeittom <gilmottiez@gmail.com>
2019-04-18 09:29:02 +0200

    back to @20kHz for the next steps

--- Oscilloscope/Src/main.c
+++ Oscilloscope/Src/main.c
@@ -367,9 +367,9 @@ static void MX_TIM1_Init(void)
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 8;
+  htim1.Init.Prescaler = 84;
   htim1.Init.CounterMode = TIM_COUNTERMODE_DOWN;
-  htim1.Init.Period = 92;
+  htim1.Init.Period = 98;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
--- src/app/factory.h
+++ src/app/factory.h
@@ -9,7 +9,7 @@
 #include "oscilloscopecontroller.h"
 #include "gui.h"
 
-#define ADC_VALUES_BUFFER_SIZE 10000
+#define ADC_VALUES_BUFFER_SIZE 1000
 
 using oscilloscope::Gui;
 
